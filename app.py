c4c3e0eaafc4a59406bfbf13db9a54aa3246f33c
Naj Ran <najrandm@gmail.com>
2024-06-28 14:38:45 +0300

    Adding GUI for entering data

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def predict_api():
     output = model.predict(new_trans_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods = ["POST"])
+def perdict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    data_response = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text = f"The House price prediction is {data_response}")
 if __name__ == "__main__":
     app.run(debug= True)
